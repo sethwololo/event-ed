c1073ab3bb083a46a610dbd8e091e3b20184276a
sethwololo <54484095+sethwololo@users.noreply.github.com>
2019-11-19 09:59:41 -0300

    Pesquisa trocada por dropdown

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './home.css';
 //import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar/';
-//import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import firebase from '../../config/firebase';
 //import { toastSucesso, toastErro } from '../../config/toastr';
 import EventoCard from '../../components/evento-card';
@@ -10,8 +10,9 @@ import EventoCard from '../../components/evento-card';
 const Home = ({ match }) => {
 
   const [eventos, setEventos] = useState([]);
-  const [pesquisa, setPesquisa] = useState('');
-  //let listaeventos = [];
+  const [pesquisa, setPesquisa] = useState('Todos');
+  const usuarioEmail = useSelector(state => state.usuarioEmail);
+  console.log(usuarioEmail);
 
   /*useEffect(() => {
     setPesquisa('');
@@ -31,11 +32,10 @@ const Home = ({ match }) => {
   useEffect(() => {
     let unsubscribe;
     if (match.params.parametro) {
-      alert("MEUS EVENTOS")
-    } else {
       unsubscribe = firebase
         .firestore()
         .collection('eventos')
+        .where('usuario', '==', usuarioEmail)
         .onSnapshot(snapshot => {
           const listaDeEventos = snapshot.docs.map((doc) => ({
             id: doc.id,
@@ -44,9 +44,36 @@ const Home = ({ match }) => {
           setEventos(listaDeEventos);
         });
       return () => unsubscribe();
+    } else {
+      if (pesquisa === 'Todos') {
+        unsubscribe = firebase
+          .firestore()
+          .collection('eventos')
+          .onSnapshot(snapshot => {
+            const listaDeEventos = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data()
+            }));
+            setEventos(listaDeEventos);
+          });
+        return () => unsubscribe();
+      } else {
+        unsubscribe = firebase
+          .firestore()
+          .collection('eventos')
+          .where('tipo', '==', pesquisa)
+          .onSnapshot(snapshot => {
+            const listaDeEventos = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data()
+            }));
+            setEventos(listaDeEventos);
+          });
+        return () => unsubscribe();
+      }
     }
 
-  }, []);
+  }, [pesquisa]);
 
 
   console.log(eventos)
@@ -58,7 +85,12 @@ const Home = ({ match }) => {
       <Navbar />
       <div className="row p-4 mx-3 text-center row-pesquisa">
         <h1 className="mx-auto font-weight-black">Eventos</h1>
-        <input onChange={e => setPesquisa(e.target.value)} type="text" className="form-control barra-pesquisa" placeholder="Pesquisar evento" />
+        <select onChange={e => setPesquisa(e.target.value)} className="form-control barra-pesquisa">
+          <option>Todos</option>
+          <option>Palestra</option>
+          <option>Workshop</option>
+          <option>Conferência</option>
+        </select>
       </div>
       <div className="row mt-3 mx-2 p-4">
         {eventos.map(item => <EventoCard key={item.id} id={item.id} img={item.imagem} titulo={item.titulo} detalhes={item.detalhes} visualizacoes={item.visualizacoes} />)}
